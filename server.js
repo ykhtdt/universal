b1f296d787b1e9ca64998a3167bb316910f66b91
jgpark-1 <jgpark7281@gmail.com>
2024-05-11 01:57:39 +0900

    feat: use express in server.js

--- server.js
+++ server.js
@@ -1,57 +1,19 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
 const path = require("path");
 
-const port = 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true);
-  let filePath = "." + reqUrl.pathname;
+// 정적 파일을 제공하기 위해 express.static 미들웨어를 사용합니다.
+app.use(express.static(path.join(__dirname, "/")));
 
-  if (filePath === "./") {
-    filePath = "./index.html";
-  }
-
-  const extname = path.extname(filePath);
-
-  let contentType = "text/html";
-
-  switch (extname) {
-    case ".js":
-      contentType = "application/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-  }
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code == "ENOENT") {
-        fs.readFile("./index.html", (err, content) => {
-          if (err) {
-            res.writeHead(500);
-            res.end("Internal Server Error");
-          } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content, "utf-8");
-          }
-        });
-      } else {
-        res.writeHead(500);
-        res.end("Internal Server Error");
-      }
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf-8");
-    }
-  });
+// 모든 요청에 대해 index.html을 반환하는 라우트 설정
+app.get("*", (req, res) => {
+  console.log(req.url);
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+// 서버 시작
+app.listen(PORT, () => {
+  console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
