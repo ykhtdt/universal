40ea7edb1e401986fffc9b52b44a0580c7fdce59
jgpark-1 <jgpark7281@gmail.com>
2024-05-12 17:08:09 +0900

    feat: add event methods in component.js

--- src/shared/base/component.js
+++ src/shared/base/component.js
@@ -1,15 +1,52 @@
 class Component {
-  constructor(params) {
+  target;
+  props;
+  state;
+  params;
+
+  constructor({ target, props, state, params }) {
+    this.target = target;
+    this.props = props;
+    this.state = state;
     this.params = params;
+    this.initialState();
+    this.setEvent();
+    this.render();
   }
 
-  // 페이지 제목 변경
-  setTitle(title) {
-    document.title = title;
-  }
+  initialState() {}
+
+  mounted() {}
 
   async template() {
     return "";
+  }
+
+  async render() {
+    this.target.innerHTML = await this.template();
+    this.mounted();
+  }
+
+  setState(newState) {
+    this.state = {
+      ...this.state,
+      ...newState
+    };
+
+    this.render();
+  }
+
+  setEvent() {}
+
+  addEvent(eventType, selector, callback) {
+    this.target.addEventListener(eventType, (event) => {
+      // closest: 일치하는 요소를 선택자와 일치하는 요소를 찾을 때까지, 자기 자신을 포함해 위쪽으로 문서 트리를 순회
+      if (!event.target.closest(selector)) {
+        return false;
+      }
+
+      callback(event);
+    })
   }
 }
 
