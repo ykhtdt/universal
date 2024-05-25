e79bba0759aae2e2a3fe989197ecc1d81b7f229c
jgpark-1 <jgpark7281@gmail.com>
2024-05-25 19:17:06 +0900

    chore: remove console

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "/")));
 
 app.get("*", (req, res) => {
-  console.log(req.url);
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
