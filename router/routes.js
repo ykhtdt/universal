5b6bd9e7b4b4a5a758bc8c05b8cc442e6df8164a
jgpark-1 <jgpark7281@gmail.com>
2024-05-30 22:04:18 +0900

    feat: add todo page router

--- router/routes.js
+++ router/routes.js
@@ -3,8 +3,11 @@ import Home from "/src/pages/home.js";
 import Posts from "/src/pages/posts.js";
 import Post from "/src/pages/post.js";
 
+import Todo from "/src/pages/todo/page.js";
+
 const routes = [
   { path: "/", view: Home },
+  { path: "/todo", view: Todo },
   { path: "/posts", view: Posts },
   { path: "/posts/:category", view: Posts },
   { path: "/posts/:category/:id", view: Post },
