458f25c250ccee58009f1f4281d7857d6c35620e
jgpark-1 <jgpark7281@gmail.com>
2024-05-30 22:02:10 +0900

    feat: add className props to FilledButton

--- src/shared/ui/button/filled-button.js
+++ src/shared/ui/button/filled-button.js
@@ -2,14 +2,18 @@ import Component from "/src/shared/base/component.js";
 
 class FilledButton extends Component {
   template() {
-    const { id, children } = this.props;
+    const { id, className, children } = this.props;
 
     // 아래 코드는 동작하지 않는다. 템플릿 리터럴을 사용하여 HTML 문자열을 생성할 때 함수를 전달할 수 없다.
     // const onClick = this.props.onClick;
     // onClick(); // onClick is not a function
 
     return `
-      <button class="filled-button" data-button-id="${id}" data-component="filled-button">
+      <button
+        class="filled-button ${className}"
+        data-button-id="${id}"
+        data-component="filled-button"
+      >
         ${children}
       </button>
     `;
